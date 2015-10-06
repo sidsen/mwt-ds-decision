55bc3981b228024c98265dc1ceed6bd9c1993956
Luong Hoang <lhoang@live.com>
2015-10-06 13:58:26 -0400

    updated version and released nuget

--- ClientDecisionService/CreateNuGet.cmd
+++ ClientDecisionService/CreateNuGet.cmd
@@ -1,1 +1,1 @@
-..\.nuget\nuget pack ClientDecisionService.csproj -Prop "Configuration=Release;Platform=AnyCPU" -OutputDirectory . -IncludeReferencedProjects
+..\.nuget\nuget pack ClientDecisionService.csproj -Prop "Configuration=Release;Platform=x64" -OutputDirectory . -IncludeReferencedProjects
